6dda1183fa98215b2e6fc23b81211ed5fb941077
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-07 18:47:55 -0500

    step

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -36,15 +36,15 @@ class Shop {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
+        if (item.name == 'Aged Brie') {
+          if (item.quality < 50) {
+            item.quality = item.quality + 1;
+          }
+        } else {
           if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
             this.decrementItemQuality(item)
           } else {
             item.quality = item.quality - item.quality;
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
           }
         }
       }
