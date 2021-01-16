cefdfaacf15ef42a56694affc3655db9a952efc8
Iuliu Pop <iuliu.laurentiu.pop@protonmail.com>
2021-01-16 18:45:13 -0500

    step

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -10,6 +10,11 @@ function increaseQuality(item, increment = 1) {
   item.quality = Math.min(item.quality + increment, 50)
 }
 
+const agedBrie = 'Aged Brie'
+const backstagePass = 'Backstage passes to a TAFKAL80ETC concert'
+const sulfuras = 'Sulfuras, Hand of Ragnaros'
+
+
 class Shop {
   constructor(items=[]) {
     this.items = items;
@@ -18,9 +23,9 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       let item = this.items[i];
-      if (item.name == 'Aged Brie' || item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+      if (item.name == agedBrie || item.name == backstagePass) {
         increaseQuality(item)
-        if (item.name == 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.name == backstagePass) {
           if (item.sellIn < 6) {
             increaseQuality(item, 2)
           } else if (item.sellIn < 11) {
@@ -28,20 +33,20 @@ class Shop {
           }
         }
       } else {
-        if (item.name != 'Sulfuras, Hand of Ragnaros') {
+        if (item.name != sulfuras) {
           if (item.quality > 0) {
             item.quality = item.quality - 1;
           }
         }
       }
-      if (item.name != 'Sulfuras, Hand of Ragnaros') {
+      if (item.name != sulfuras) {
         item.sellIn = item.sellIn - 1;
       }
       if (item.sellIn < 0) {
-        if (item.name != 'Aged Brie') {
-          if (item.name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (item.name != agedBrie) {
+          if (item.name != backstagePass) {
             if (item.quality > 0) {
-              if (item.name != 'Sulfuras, Hand of Ragnaros') {
+              if (item.name != sulfuras) {
                 item.quality = item.quality - 1;
               }
             }
